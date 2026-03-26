81bc8c8c615ce74a8b32b79c897cadd48989f272
schmettow <schmettow@web.de>
2026-03-26 18:41:48 +0100

    sc

--- ylab-lib/src/mod.rs
+++ ylab-lib/src/mod.rs
@@ -1,13 +1,10 @@
 #![no_std]
 
+pub use core::fmt::Write;
 pub use core::sync::atomic::AtomicBool;
 pub use core::sync::atomic::Ordering;
-pub use heapless::{String, Vec};
-pub use static_cell::StaticCell;
-pub use core::fmt::Write;
-pub use log;
-pub use defmt::println;
 pub use defmt::Format;
+pub use defmt::println;
 pub use embassy_embedded_hal as ehal;
 pub use embassy_executor::Spawner;
 pub use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex as RawMutex;
@@ -16,6 +13,9 @@ pub use embassy_sync::channel::Channel;
 pub use embassy_sync::mutex::Mutex;
 pub use embassy_sync::signal::Signal;
 pub use embassy_time as time;
+pub use heapless::{String, Vec};
+pub use log;
+pub use static_cell::StaticCell;
 
 pub use time::{Delay, Duration, Instant, Ticker, Timer};
 pub static ORD: Ordering = Ordering::SeqCst;
@@ -23,8 +23,8 @@ pub static ORD: Ordering = Ordering::SeqCst;
 pub mod ybus;
 pub mod ydata;
 pub mod ysns;
-pub mod yuii;
 pub mod ytfk;
+pub mod yuii;
 pub mod yuio;
 
 //pub use ybus::SharedI2cBus;
